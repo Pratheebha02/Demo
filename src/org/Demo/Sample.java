501e8497a74c1f2b4580d74280c6cc49dacd205f
Pratheebha02 <smprathee@gmail.com>
2023-09-28 10:04:34 +0800

    add

--- src/org/Demo/Sample.java
+++ src/org/Demo/Sample.java
@@ -1,6 +1,12 @@
 package org.Demo;
 
 public class Sample {
+	
+	public void method1() {
+		
+		System.out.println("Git added successfully");
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
